a8eea438350c66408b4ed38917cba5ed8837760d
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-17 09:01:59 +0800

    Update project

---
+++ backend/run_reg.bat
@@ -0,0 +1,5 @@
+
+@echo off
+cd /d d:\SOLO-2\AI_solo_coder_task_A_136\backend
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "d:\SOLO-2\AI_solo_coder_task_A_136\backend\regression_test.ps1"
+echo DONE
